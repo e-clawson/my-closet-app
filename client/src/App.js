6433eb8433e04a8f4e05e06018f8d3d340ca5ddd
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-16 17:19:39 +0530

    added header to homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Header from "./Header";
 
 function App() {
   return (
--- client/src/Header.js
+++ client/src/Header.js
@@ -3,20 +3,18 @@ import styled from "styled-components";
 
 const Header = ({storename, slogan}) => {
   return (
-    <Wrapper>
       <div>
         <h1> {storename} </h1>
         <h2> {slogan} </h2>
       </div>
-    </Wrapper>
   )
 }
 
-const Wrapper = styled.header`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 8px;
-`;
+// const Wrapper = styled.header`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   padding: 8px;
+// `;
 
 export default Header;
