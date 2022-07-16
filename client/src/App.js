6a5902b6382a75e8246d4aca19c5819964e2cf6b
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-16 17:22:53 +0530

    install react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import { useState, useEffect } from "react";
 import Header from "./Header";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div classname="App">
+      <Logo>
+        <Link to="/"> AnthologyApp </Link>
+      </Logo>
       {/* <Navbar /> */}
       <Header slogan="Organize your outfits!"/>
 
---
+++ client/src/NavBar.js
@@ -0,0 +1,88 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { Button } from "../styles";
+import { UserContext } from "../context/user";
+
+const NavBar = () => {
+  const {user} = useContext(UserContext)
+
+  return (
+   <>
+      <Logo>
+        <Link to="/"> AnthologyApp </Link>
+      </Logo>
+      <br/>
+      <Wrapper>
+      <Nav>
+        <Button as={Link} to="/">
+         Home
+        </Button>
+        <Button as={Link} to="/about">
+         About
+        </Button>
+        {user ? (
+          <>
+            <Button as={Link} to="/newprompt">
+              New Prompt
+            </Button>
+            <Button as={Link} to="/newstory">
+              New Story
+            </Button>
+            <Button as={Link} to="/profile">
+              My Profile
+            </Button>
+            <Button as={Link} to="/logout">
+              Logout
+            </Button>
+          </>
+          ) : (
+            <>
+              <Button as={Link} to="/login">
+              Login
+              </Button>
+              <Button as={Link} to="/signup">
+              Signup
+              </Button>
+            </>
+          )
+        }
+      </Nav>
+      </Wrapper>
+    </>
+  )
+}
+
+const Wrapper = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 8px;
+`;
+
+const Logo = styled.h1`
+  font-family: "Permanent Marker", cursive;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 8px;
+  font-size: 3rem;
+  color: deeppink;
+  margin: 0;
+  line-height: 1;
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  gap: 4px;
+  justify-content: center;
+  align-items: center;
+  right: 8px;
+`;
+
+export default NavBar;
