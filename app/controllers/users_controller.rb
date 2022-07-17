1011a74af956c4e02c83863afaeb9b3420b0eb0c
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-17 18:54:03 +0530

    troubleshooting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    skip_before_action :authorized!, only: [:create ]
+    skip_before_action :authorized!, only: [:create]
   
     def create 
       user = User.create!(user_params)
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     private
   
     def user_params
-      params.permit(:username, :first_name, :last_name, :email, :password, :password_confirmation)
+      params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
   
   end
