e345befab122d7b1afd8f722421598c8ea377743
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-17 16:21:22 +0530

    add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  resources :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/hello', to: 'application#hello_world'
+  #get '/hello', to: 'application#hello_world'
+  scope :api do
+    scope :v1 do
+      resources :users, only: [:update, :destroy] #creating show route manually with a custom route
+      post "/signup", to: "users#create"
+      get "/me", to: "users#show"
+      post "/login", to: "sessions#create"
+      delete "/logout", to: "sessions#destroy"
+
+
+    end
+  end
 end
