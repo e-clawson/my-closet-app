2eb53830c21b07ef59fd2ec2d6e0b39e2a9aa35c
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-17 17:22:28 +0530

    add routes in SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+
+    def create
+        user = User.find_by_email(params[:email])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id #line responsible for signing user in 
+            render json: UserSerializer.new(user), status: :accepted
+        else
+            render json: {error: "Not Authorized"}, status: :unauthorized
+       end
+    end
+
+    def destroy
+        session.delete(:user_id)
+        head :no_content 
+    end
+
 end
