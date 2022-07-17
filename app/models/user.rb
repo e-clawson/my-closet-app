a9e45008c47e481c0ddd88e9fd570c873a6251fa
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-17 16:15:10 +0530

    add sessions controller and seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+   #has_many :items, dependent: :destroy 
+   #has_many :outfits, dependent: :destroy 
+   #has_many :collections, dependent: :destroy 
+
+    has_secure_password
+
+    # validations
+    validates :username, uniqueness: true, presence: true, length: {in: 4..25}
+    validates :email, presence: true, uniqueness: true, format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
+    validates :password, length: {in: 6..25}
 end
--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div classname="App">
+    <div className="App">
         <h2>My Closet App </h2> 
       {/* <Navbar /> */}
       <Header slogan="Organize your outfits!"/>
