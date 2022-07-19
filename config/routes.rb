3093d9a1895710e4b11e87531f93da99b2de7849
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-19 14:36:07 +0530

    add omniauth route to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,17 @@ Rails.application.routes.draw do
   #get '/hello', to: 'application#hello_world'
   scope :api do
     scope :v1 do
+
+      #will be a get and not post -
+      #This route will direct any auth callback response to our SessionsController omniauth action with params[:provider] set to google_oauth2 in this example.
+      get "/auth/:provider/callback", to: "sessions#omniauth"
+
       resources :users, only: [:update, :destroy] #creating show route manually with a custom route
       post "/signup", to: "users#create"
       get "/me", to: "users#show"
       post "/login", to: "sessions#create"
       delete "/logout", to: "sessions#destroy"
+
 
 
     end
