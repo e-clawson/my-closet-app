a5d7c8e1f76c582cc0f88adc7f0a5a0099ecb6cb
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-19 14:30:48 +0530

    add middleware trigger to frontend login page for omniauth

---
+++ client/src/components/user/Login.js
@@ -0,0 +1,3 @@
+// Anywhere that you want to add the option for a user to “Log in with Google”, you will want to add this code to your view file:
+
+// <%= button_to "Log in with Google", '/auth/google_oauth2'  %>
