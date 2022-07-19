cc4e0a0e198fc575fd878026edfed79a9a69c235
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-19 14:52:18 +0530

    add uid colomn to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_16_122649) do
+ActiveRecord::Schema.define(version: 2022_07_19_092034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2022_07_16_122649) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uid"
+    t.string "provider"
   end
 
 end
