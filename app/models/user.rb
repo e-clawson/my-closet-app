1b7905e81fec167149c56499f133bf2a2337739e
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-19 14:59:26 +0530

    add .from_omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,14 @@ class User < ApplicationRecord
     validates :username, uniqueness: true, presence: true, length: {in: 4..25}
     validates :email, presence: true, uniqueness: true, format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
     validates :password, length: {in: 6..25}
+
+    def self.from_omniauth(response)
+        User.find_or_create_by(uid: response[:uid], provider: response[:provider]) do |u|
+            u.first_name = response[:info][:first_name]
+            u.last_name = response[:info][:last_name]
+            u.email = response[:info][:email]
+            u.password = SecureRandom.hex(15)
+        end
+    end
+    
 end
