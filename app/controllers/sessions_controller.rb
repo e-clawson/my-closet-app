f8a5b93e1fbaf1c65469f7e0147902d7b2b8e4c7
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-19 14:41:06 +0530

    add omnitauth route to sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,17 @@
 class SessionsController < ApplicationController
     skip_before_action :authorized!, only: [:create]
 
+    def omniauth
+        user =User.from_omniauth(request.env['omniauth.auth'])
+        if user.valid?
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else 
+            redirect_to '/login'
+        end
+    
+    end
+
     def create
         user = User.find_by_email(params[:email])
         if user&.authenticate(params[:password])
--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,6 @@ Rails.application.routes.draw do
       post "/login", to: "sessions#create"
       delete "/logout", to: "sessions#destroy"
 
-
-
     end
   end
 end
