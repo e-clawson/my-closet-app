4a4dcfdee182aaf4e82a807a77ba67aba7f1c87d
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-21 13:40:20 +0530

    add signup page

---
+++ client/src/components/user/Signup.js
@@ -0,0 +1,101 @@
+import React, { useState, useContext } from "react";
+import { Button, Input, FormField, Label } from "../../styles";
+import {useHistory} from "react-router-dom"
+import { UserContext } from "../../context/user";
+import { MessageContext } from "../../context/message";
+
+const SignUpForm = () => {
+  const {signup} = useContext(UserContext);
+  const {setMessage} = useContext(MessageContext);
+  
+  const [userObj, setUserObj] = useState({
+      email: "",
+      firstName: "",
+      lastName: "",
+      password: "",
+      passwordConfirmation: ""
+  });
+
+  const history = useHistory()
+
+  const handleChange = ({target: {name, value}}) => {
+      setUserObj({
+          ...userObj,
+          [name]: value
+      })
+  }
+
+  const handleSubmit = e => {
+      e.preventDefault()
+      if ([userObj.firstName, userObj.lastName, userObj.email, userObj.password, userObj.passwordConfirmation].some(val => val.trim() === "")) {
+          setMessage({message: "You must fill in all the information please!", color: "red"})
+      }
+      const didItWork = signup({...userObj, password_confirmation: userObj.passwordConfirmation})
+      if (didItWork) {
+          setMessage({message: "User successfully created!", color: "green"})
+          history.push("/profile")
+      }
+      
+      
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <FormField>
+        <Label htmlFor="firstName">First Name</Label>
+        <Input
+          type="text"
+          name="firstName"
+          autoComplete="off"
+          value={userObj.firstName}
+          onChange={handleChange}
+        />
+      </FormField>
+      <FormField>
+        <Label htmlFor="lastName">Last Name</Label>
+        <Input
+          type="text"
+          name="lastName"
+          autoComplete="off"
+          value={userObj.lastName}
+          onChange={handleChange}
+        />
+      </FormField>
+      <FormField>
+        <Label htmlFor="email">Email</Label>
+        <Input
+          type="text"
+          name="email"
+          autoComplete="off"
+          value={userObj.email}
+          onChange={handleChange}
+        />
+      </FormField>
+      <FormField>
+        <Label htmlFor="password">Password</Label>
+        <Input
+          type="password"
+          name="password"
+          value={userObj.password}
+          onChange={handleChange}
+          autoComplete="current-password"
+        />
+      </FormField>
+      <FormField>
+        <Label htmlFor="passwordConfirmation">Password Confirmation</Label>
+        <Input
+          type="password"
+          name="passwordConfirmation"
+          value={userObj.passwordConfirmation}
+          onChange={handleChange}
+          autoComplete="current-password"
+        />
+      </FormField>
+      <FormField>
+        <Button type="submit">Sign Up</Button>
+      </FormField>
+    </form>
+  );
+}
+
+export default SignUpForm;
