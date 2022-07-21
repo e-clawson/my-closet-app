f251632fceb37f62009561124dee41f944b1bc42
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-21 13:35:37 +0530

    add logout.js

---
+++ client/src/components/user/Logout.js
@@ -0,0 +1,26 @@
+import {useEffect, useContext} from 'react'
+import {UserContext} from '../../context/user.js'
+import {useHistory} from 'react-router-dom'
+
+function Logout() {
+   const {logout} = useContext(UserContext)
+   const history = useHistory()
+
+   useEffect(() => {
+       const handleLogout = async () => {
+           const onLogout = await logout()
+           if (onLogout) {
+               history.push("/login")
+           }
+       }
+       handleLogout()
+   }, [logout, history]);
+
+   return (
+       <div> Logging Out... </div>
+   )
+};
+
+
+
+export default Logout
