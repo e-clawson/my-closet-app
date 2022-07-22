2c85b6ad5beb25e6dddbf4ccee67225194306788
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-22 15:04:40 +0530

    add image assoc and validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,22 @@ class Item < ApplicationRecord
     has_many :outfit_items
     has_many :outfits, through: :outfit_items
 
+    #image assoc
+    has_one_attached :item_image
+
+    #image validations
+    validate :acceptable_image
+
+    def acceptable_image
+        return unless item_image.attached?
+      
+        unless item_image.byte_size <= 1.megabyte
+          errors.add(:item_image, "is too big")
+        end
+      
+        acceptable_types = ["image/jpeg", "image/png"]
+        unless acceptable_types.include?(item_image.content_type)
+          errors.add(:item_image, "must be a JPEG or PNG")
+        end
+    end
 end
