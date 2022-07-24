db8ed6ae5fcb10ee20e16b2b71ba2b8c41468245
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-24 15:17:50 +0530

    update

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,12 +1,12 @@
 class ItemSerializer #
   include JSONAPI::Serializer
-  attributes :id, :name, :item_type, :size, :color, :description, :user_id
+  attributes :id, :name, :item_type, :size, :color, :description, :user_id, :item_image_format, :attachment_format
 
   def item_image_format
     return unless object.avater.attached?
-    object.avatar.blob.attributes
+    object.item_image.blob.attributes
       .slice('filename', 'byte_size')
-      .merge(url: object.image_url)
+      .merge(url: object.image_url) 
       .tap { |attrs| attrs['name'] = attrs.delete('filename') }
   end 
 
@@ -18,5 +18,5 @@ class ItemSerializer #
     .merge(url: object.image_url)
     .tap { |attrs| attrs['name'] = attrs.delete('filename') }
   end 
-  
+
 end
