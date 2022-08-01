eda84513dc81aecbdbd5b70cdd4781c36ef6766c
Elizabeth Clawson <emclawson1@gmail.com>
2022-08-01 13:41:32 +0530

    create outfitItemList.js

--- client/src/components/outfitItems/OutfitItemList.js
+++ client/src/components/outfitItems/OutfitItemList.js
@@ -1,33 +1,13 @@
-import ItemCard from "../items/ItemCard"
-import {useParams} from "react-router-dom"
-import {useState, useEffect} from "react"
+import OutfitItemCard from "./OutfitItemCard.js"
 
-const OutfitItemList = ({items, handleError}) => {
-    const {outfitId} = useParams()
-    const [itemsList, setItemsList] = useState(null)
+const OutfitItemList = ({outfitItems}) => {
+ const outfitItemCards = outfitItems?.map(outfitItem => <OutfitItemCard key={outfitItem.id} outfitItem={outfitItem}/>)
     
-    useEffect(() => {
-        if (!outfits) {
-            fetch(`api/v1/${outfitId}/items`)
-            .then(resp => {
-                if (resp.status === 200) {
-                    resp.json()
-                    .then(items => setItemsList(items))
-                } else {
-                    resp.json()
-                    .then(errorObj => handleError(errorObj.error))
-                }
-            })
-            .catch(error => handleError(error))
-        }
-    }, [outfitId, items, handleError])
-
-    // if (!comments) return <h2>The data you tried to access does not exist!</h2>
-    const finalItemsList = items ? items : itemsList
-    const renderItems = finalItemsList?.map(item => <ItemCard key={item.id} item={item}/>)
-    return (
-        <div>{renderItems}</div>
-    )
+ return (
+    <div>
+      <div style = {{display:'flex', flexWrap:'wrap'}}>{outfitItemCards}</div>
+    </div>
+  )
 }
 
-export default OutfitItemList
+export default OutfitItemList;
